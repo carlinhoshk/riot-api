5e90cd2f77928a5a53bd638ad4aa7b42ea6c3355
carlinhoshk <carlosmdohk@gmail.com>
2023-08-05 10:59:56 -0300

    feat: First commit, env, infobyName

---
+++ infoByName.py
@@ -0,0 +1,15 @@
+import requests, os
+
+api_key = os.environ['RIOT_KEY']
+summoner_name = input('Nome do Jogador: ')
+
+url = f'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}'
+headers = {'X-Riot-Token': api_key}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    summoner_data = response.json()
+    print(summoner_data)
+else:
+    print('Erro na solicitação:', response.status_code)
