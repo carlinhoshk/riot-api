f6dc9a0c9673f99101bc24e718d3cc35692a03cf
carlinhoshk <carlosmdohk@gmail.com>
2023-08-06 14:27:03 -0300

    chore: infobyName try use request riot-api

--- infoByName.py
+++ infoByName.py
@@ -3,18 +3,16 @@ import os
 #from decouple import config ( old build, new give a virtualenv with path variables )
 
 
-
+name = input('Nome do Jogador: ')
+url = f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}"
 api_key = os.getenv('RIOT_KEY')
+params = {'api_key': api_key}
 
-summoner_name = input('Nome do Jogador: ')
+try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-url = f'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}'
-headers = {'X-Riot-Token': api_key}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
     summoner_data = response.json()
     print(summoner_data)
-else:
-    print('Erro na solicitação:', response.status_code)
+except requests.exceptions.RequestException as e:
+    print('Erro na solicitação:', e)
